cf5b03e857af0f6ed9988e53dcd05fb9bd931323
Rubenabdias1 <ruben_abdias@live.com>
2020-05-31 00:41:40 -0500

    Adding header and styles to home

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,10 +4,10 @@ import CollectionPreview from "../../components/collection-preview/collection-pr
 class ShopPage extends Component {
   constructor(props) {
     super(props);
-    this.state = SHOP_DATA;
+    this.state = { collections: SHOP_DATA };
   }
   render() {
-    const collections = this.state;
+    const { collections } = this.state;
     return (
       <div className="shop-page">
         {collections.map(({ id, ...otherCollectionProps }) => (
