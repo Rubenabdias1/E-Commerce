9dc03900e2a5b0729f307d7db93f16608c76a20e
Rubenabdias1 <ruben_abdias@live.com>
2020-06-13 00:11:03 -0500

    Set up redux persistor

--- src/App.jsx
+++ src/App.jsx
@@ -62,4 +62,5 @@ const mapStateToProps = createStructuredSelector({
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(App);
