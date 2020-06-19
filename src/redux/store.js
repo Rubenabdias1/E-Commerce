94d420cdb459a9dd09ad476e5a391a518e626f2e
Rubenabdias1 <ruben_abdias@live.com>
2020-06-18 22:39:41 -0500

    disable redux dev tools in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,9 @@ import rootReducer from "./root-reducer";
 
 export const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  process.env.NODE_ENV === "development" &&
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export const persistor = persistStore(store);
