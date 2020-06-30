844a8eb6a8cb8bba4e64024c269054d9aea52750
Rubenabdias1 <ruben_abdias@live.com>
2020-06-29 21:12:52 -0500

    Move data to firestore, create update collections data and reducer and fetch collections on shop page component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -36,6 +36,35 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 };
 firebase.initializeApp(config);
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    console.log(newDocRef);
+    batch.set(newDocRef, obj);
+  });
+  return await batch.commit();
+};
+
+export const convertCollectionSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+    return {
+      id: doc.id,
+      title,
+      items,
+      routeName: encodeURI(title.toLowerCase()),
+    };
+  });
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { updateCollections } from "../../redux/shop/shop.actions";
 import collectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CategoryPage from "../../components/collection/collection.component";
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={collectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
-  </div>
-);
+class ShopPage extends Component {
+  unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+
+    collectionRef.onSnapshot(async (snapshot) => {
+      const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div className="shop-page">
+        <Route exact path={`${match.path}`} component={collectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
+      </div>
+    );
+  }
+}
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collections) => dispatch(updateCollections(collections)),
+});
+export default connect(null, mapDispatchToProps)(ShopPage);
